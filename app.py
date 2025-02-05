085d5943f40499d4b2aea8575a8996caf2d530e6
Diya Jain <diyajainb17@gmail.com>
2025-02-05 14:53:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 import streamlit as st
 import pickle
 import string
+import os
 from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
 
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('wordnet')
+nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
+os.makedirs(nltk_data_path, exist_ok=True)
+nltk.data.path.append(nltk_data_path)
+
+nltk.download('punkt', download_dir=nltk_data_path)
+nltk.download('stopwords', download_dir=nltk_data_path)
+nltk.download('wordnet', download_dir=nltk_data_path)
 
 ps = PorterStemmer()
 
