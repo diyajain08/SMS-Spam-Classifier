2ac7a8e17e860fed673cec880617313fa2d06e2b
Diya Jain <diyajainb17@gmail.com>
2025-02-05 15:22:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,20 +3,21 @@ import pickle
 import string
 import nltk
 import os
+import shutil
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
 
-# Set a fixed nltk_data path
 nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
-os.makedirs(nltk_data_path, exist_ok=True)  # Create directory if it doesn't exist
-nltk.data.path.append(nltk_data_path)  # Add path to nltk
+if os.path.exists(nltk_data_path):
+    shutil.rmtree(nltk_data_path)  # Deletes the folder to avoid old corrupted files
 
-# Ensure necessary NLTK resources are downloaded at runtime
+os.makedirs(nltk_data_path, exist_ok=True)
+nltk.data.path.append(nltk_data_path)
+
 nltk.download('punkt', download_dir=nltk_data_path)
 nltk.download('stopwords', download_dir=nltk_data_path)
 nltk.download('wordnet', download_dir=nltk_data_path)
 
-# Force NLTK to load the correct tokenizer
 try:
     nltk.data.find('tokenizers/punkt')
 except LookupError:
