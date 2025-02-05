bca869b1b4ba470d53b46538b851073c8d84b15e
Diya Jain <diyajainb17@gmail.com>
2025-02-05 15:05:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,12 @@ import os
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
 
-nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
-os.makedirs(nltk_data_path, exist_ok=True)
-nltk.data.path.append(nltk_data_path)
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
 
-nltk.download('punkt', download_dir=nltk_data_path)
-nltk.download('stopwords', download_dir=nltk_data_path)
-nltk.download('wordnet', download_dir=nltk_data_path)
+# Force NLTK to recognize the tokenizer
+nltk.data.find('tokenizers/punkt')
 
 ps = PorterStemmer()
 
